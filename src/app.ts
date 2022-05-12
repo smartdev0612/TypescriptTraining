eccd3325d197f3844e513e9ce74824d0b54440c4
Daniel Morales <danieldev0612@gmail.com>
2022-05-12 12:38:50 -0700

    overriding properties

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Department {
     // private name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {
         // this.name = n;
@@ -20,10 +20,52 @@ class Department {
     }
 }
 
-const accounting = new Department('id', 'Accounting');
+class ITDepartment extends Department {
+    admins: string[];
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Account');
+    }
+
+    addEmployee(name: string): void {
+        if(name === 'Max') {
+            return;
+        }
+
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReport() {
+        console.log(this.reports);
+    }
+}
+
+const it = new ITDepartment('id', ['Max']);
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+it.describe();
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Soomething went wrong...');
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 
-accounting.describe();
+accounting.printReport();
 accounting.printEmployeeInformation();
