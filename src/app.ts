5b3d45649c4081b0cbaa31cc7731584679246206
Daniel Morales <danieldev0612@gmail.com>
2022-05-13 19:43:20 -0700

    generics & union types

--- src/app.ts
+++ src/app.ts
@@ -194,3 +194,60 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({name: 'Max'}, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objectStorage = new DataStorage<object>();
+// const maxObj = {name: 'Max'};
+// objectStorage.addItem({name: 'Max'});
+// objectStorage.addItem({name: 'Manu'});
+// // ...
+// objectStorage.removeItem({name: "Max"});
+// console.log(objectStorage);
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string, 
+    description: string, 
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Sports'];
+// names.push('Manu');
+// names.pop();
